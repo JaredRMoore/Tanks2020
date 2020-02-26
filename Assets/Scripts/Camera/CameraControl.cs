4b59c361cc56e5d42a308021d3ec0719967766da
s200078 <s200078@ADL-A4AC86>
2020-02-26 20:14:41 +1030

    Fixed camera issues and shell spawns

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -7,13 +7,37 @@ public class CameraControl : MonoBehaviour
     public float m_DampTime = 0.2f;
 
     public Transform m_target;
-
     private Vector3 m_MoveVelocity;
     private Vector3 m_DesiredPosition;
+    public float zoomSpeed = 10;
+    public float rotateSpeed = 1;
 
     private void Awake()
     {
         m_target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    
+    private void Update()
+    {
+        // Scroll the camera in and out
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 cameraPos = Camera.main.transform.localPosition;
+        cameraPos.z += scroll * zoomSpeed;
+        cameraPos.z = Mathf.Clamp(cameraPos.z, -30, -5);
+        Camera.main.transform.localPosition = cameraPos;
+
+        // Move the camera in right mouse drag
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 angles = transform.localEulerAngles
+                + (Vector3.right * Input.GetAxis("Mouse Y")
+                + Vector3.up * Input.GetAxis("Mouse X")) *rotateSpeed;
+
+            angles.x = Mathf.Clamp(angles.x, 0, 80);
+
+            transform.localEulerAngles = angles;
+        }
     }
 
     private void FixedUpdate()
