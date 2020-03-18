2b1969b3f0511066af0f9e9a27bde2045ad370cc
s200078 <s200078@ADL-A4AC86>
2020-03-18 19:41:16 +1030

    Fixed tank movements & power ups

---
+++ Assets/Scripts/PowerUps/SpeedPowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    public float duration = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Destroy the game object
+            Destroy(gameObject);
+
+            // If it's the player, double their speed
+            TankMovement tankMovement = other.GetComponent<TankMovement>();
+            if (tankMovement != null)
+            {
+                tankMovement.doubleSpeedTimer = duration;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    
+    }
+}
--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankMovement : MonoBehaviour
 {
-    public float m_Speed = 12f;         // How fast the tank moves forward and back
+    public float m_Speed = 6f;         // How fast the tank moves forward and back
     public float m_TurnSpeed = 180f;    // How fast the tank turns in degrees per second
 
     private Rigidbody m_Rigidbody;
     private float m_MovementInputValue; // The current value of the movement input
     private float m_TurnInputValue;     // The current value of the turn input
+
+    public float doubleSpeedTimer;
+    public GameObject speedTrail;
+
+    public Image speedClock;
 
     private void Awake()
     {
@@ -45,9 +51,33 @@ public class TankMovement : MonoBehaviour
     }
     private void Move()
     {
+        float speedFactor = 1;
+
+        if (doubleSpeedTimer > 0)
+        {
+            (doubleSpeedTimer) -= Time.deltaTime;
+            speedFactor = 2;
+
+            if (speedClock != null)
+            {
+                speedClock.fillAmount = doubleSpeedTimer / 10.0f;
+            }
+            if (speedTrail != null)
+            {
+                speedTrail.SetActive(true);
+            }
+        }
+        else
+        {
+            if (speedTrail != null)
+            {
+                speedTrail.SetActive(false);
+            }
+        }
+
         // Create a vector in the direction the tank is facing with a magnitude
         // based on the input, speed and time between frames
-        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime * speedFactor;
 
         // Apply this movement to the Rigidbody's position
         m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
